1d27e88a2a0714a651eb7ce2b679c1423f043252
Margarita Mollova <mmollova@vmware.com>
2023-01-19 17:52:17 +0200

    Changed the scritps from postgres to mysql dialect

---
+++ dbScripts/README.md
@@ -0,0 +1,4 @@
+1. `brew install mysql`
+2. `mysql -u root`
+3. `mysql> create database book_club;`
+4. `mysql> exit;`
--- dbScripts/initDb.sql
+++ dbScripts/initDb.sql
@@ -16,23 +16,28 @@ create table resources (
 );
 
 create table resources_taken (
-	resource_id int not null, 
-	user_id int not null,
+	resource_id bigint unsigned not null, 
+	user_id bigint unsigned not null,
 	date_taken timestamp not null default now(),
-	date_to_return timestamp not null default now(),
+	date_to_return timestamp,
 	primary key (resource_id, user_id),
-	constraint fk_resource foreign key (resource_id) references resources(resource_id),
-	constraint fk_user foreign key (user_id) references users(user_id)
+	constraint fk_resource_taken foreign key (resource_id) references resources(resource_id),
+	constraint fk_user_taken foreign key (user_id) references users(user_id)
 );
 
 create table rates (
-	resource_id int not null,
-	user_id int not null, 
+	resource_id bigint unsigned not null,
+	user_id bigint unsigned not null, 
 	rate int not null check (rate >= 1 and rate <= 10),
 	primary key (resource_id, user_id),
-	constraint fk_resource foreign key (resource_id) references resources(resource_id),
-	constraint fk_user foreign key (user_id) references users(user_id)
+	constraint fk_resource_rate foreign key (resource_id) references resources(resource_id),
+	constraint fk_user_rate foreign key (user_id) references users(user_id)
 );
+
+-- alter table resources_taken drop foreign key fk_resource_taken;
+-- alter table resources_taken drop foreign key fk_user_taken;
+-- alter table rates drop foreign key fk_resource_rate;
+-- alter table rates drop foreign key fk_user_rate;
 
 -- drop table if exists users cascade;
 -- drop table if exists resources cascade;
@@ -42,71 +47,47 @@ create table rates (
 
 -- TRIGGERS
 
-create function increase_readers() 
-returns trigger as $increase_readers$
-begin
-	update resources 
-	set times_read = times_read + 1,
-		current_readers = current_readers + 1
-	where resources.resource_id = new.resource_id;
-	return null;
-end;
-$increase_readers$ language plpgsql;
-
 create trigger increase_readers
 after insert on resources_taken
-for each row execute procedure increase_readers();
-
-
-create function decrease_current_readers() 
-returns trigger as $decrease_current_readers$
-begin
-	update resources 
-	set current_readers = current_readers - 1
-	where resources.resource_id = old.resource_id;
-	return null;
-end;
-$decrease_current_readers$ language plpgsql;
+for each row
+	update resources
+	set times_read = times_read + 1,
+		current_readers = current_readers + 1
+	where resource_id = new.resource_id;
 
 create trigger decrease_current_readers 
 after delete on resources_taken
-for each row execute procedure decrease_current_readers();
+for each row
+	update resources 
+	set current_readers = current_readers - 1
+	where resources.resource_id = old.resource_id;
 
-
-create function add_date_to_return() 
-returns trigger as $add_date_to_return$
-begin
-	update resources_taken
-	set date_to_return = 
-	 (select date_taken + make_interval(
-	  days := (select max_reading_days from resources
-			   where resource_id = new.resource_id))
-	  from resources_taken
-	  where resource_id = new.resource_id and user_id = new.user_id)
-	where resource_id = new.resource_id and user_id = new.user_id;
-	return null;
-end;
-$add_date_to_return$ language plpgsql;
 
 create trigger add_date_to_return 
-after insert on resources_taken
-for each row execute procedure add_date_to_return();
+before insert on resources_taken
+for each row
+	set new.date_to_return = 
+		(new.date_taken + 
+			interval (select max_reading_days
+					  from resources 
+					  where resource_id = new.resource_id) day);
+	
+
+-- drop trigger if exists increase_readers;
+-- drop trigger if exists decrease_current_readers;
+-- drop trigger if exists add_date_to_return;
 
 
--- drop trigger if exists increase_readers on resources_taken cascade;
--- drop function if exists increase_readers cascade;
+-- SCHEDULER
+	
+set global event_scheduler = on;
 
--- drop trigger if exists decrease_current_readers on resources_taken cascade;
--- drop function if exists decrease_current_readers cascade;
+create event clean_up_expired_resources
+on schedule every 1 minute
+starts current_timestamp
+ends current_timestamp + interval 1 year
+do
+	delete from resources_taken
+	where date_to_return < now();
 
--- drop trigger if exists add_date_to_returns on resources_taken cascade;
--- drop function if exists add_date_to_return cascade;
-
-
--- CRON JOB
-
-create extension pg_cron;
-
-select cron.schedule('clean up expired resources', '1 * * * *',
-	$$delete from resources_taken
-	  where date_to_return < now()$$);
+-- drop event clean_up_expired_resources;
