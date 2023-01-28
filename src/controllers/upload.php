07384f646a5aee6fc764bc035fbb6d6f168bc60a
Margarita Mollova <mmollova@vmware.com>
2023-01-29 00:45:17 +0200

    Trying to upload file

--- src/controllers/upload.php
+++ src/controllers/upload.php
@@ -1,44 +1,93 @@
 <?php
-    require_once "./connect.php";
-    require_once "../globals/queries.php";
+$target_dir = "../../uploadedFiles/";
+$target_file = $target_dir . basename($_FILES["file"]["name"]);
+$uploadOk = true;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if (isset($_POST["submit"])) {
 
-    session_start();
-
-    // check if authorized
-    if (!isset($_SESSION["valid"])) {
-        http_response_code(401);
-        exit('<p class="error">Unathorized!</p>');
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        echo "Sorry, file already exists.";
+        $uploadOk = false;
     }
 
-    if ($_SERVER["REQUEST_METHOD"] != "POST") {
-        http_response_code(400);
-        exit('<p class="error">Unexpected call!</p>');
+    // Check file size
+    if ($_FILES["file"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = false;
     }
 
-    $link = $_POST['link'];
-    $title = $_POST['title'];
-    $author = $_POST["author"];
-    $maxReaders = $_POST["max-readers"];
-    $maxDays = $_POST["max-days"];
+    //   // Allow certain file formats
+    //   if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    //   && $imageFileType != "gif" ) {
+    //     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    //     $uploadOk = 0;
+    //   }
 
-    $insertQuery = $db->prepare(UPLOAD_RESOURCE);
-    if (!$insertQuery) {
-        http_response_code(500);
-        exit("Error preparing the statement.");
+    // Check if $uploadOk is set to 0 by an error
+    if (!$uploadOk) {
+        echo "Sorry, your file was not uploaded.";
+        // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+            echo "The file " . htmlspecialchars(basename($_FILES["file"]["name"])) . " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
     }
 
-    $insertQuery->bindParam(LINK_PARAM, $link, PDO::PARAM_STR);
-    $insertQuery->bindParam(TITLE_PARAM, $title, PDO::PARAM_STR);
-    $insertQuery->bindParam(AUTHOR_PARAM, $author, PDO::PARAM_STR);
-    $insertQuery->bindParam(MAX_READERS_PARAM, $maxReaders, PDO::PARAM_INT);
-    $insertQuery->bindParam(MAX_DAYS_PARAM, $maxDays, PDO::PARAM_INT);
+}
 
-    if (!$insertQuery->execute()) {
-        http_response_code(500);
-        exit('<p class="error">Something went wrong!</p>');
-    }
 
-    echo "Successfully uploaded resource!";
 
-    exit();
+//     require_once "./connect.php";
+//     require_once "../globals/queries.php";
+
+//     session_start();
+
+//     // check if authorized
+//     if (!isset($_SESSION["valid"])) {
+//         http_response_code(401);
+//         exit('<p class="error">Unathorized!</p>');
+//     }
+
+//     if ($_SERVER["REQUEST_METHOD"] != "POST") {
+//         http_response_code(400);
+//         exit('<p class="error">Unexpected call!</p>');
+//     }
+
+//     $link = $_POST['link'];
+//     $title = $_POST['title'];
+//     $author = $_POST["author"];
+//     $maxReaders = $_POST["max-readers"];
+//     $maxDays = $_POST["max-days"];
+
+//     // $filename = $_FILES['file']['name'];
+// print_r($_FILES);
+// // echo  "size = " . count($_FILES);
+//     foreach ($_FILES as $file ) {
+//         echo "--" . $file;
+//     }
+
+//     $insertQuery = $db->prepare(UPLOAD_RESOURCE);
+//     if (!$insertQuery) {
+//         http_response_code(500);
+//         exit("Error preparing the statement.");
+//     }
+
+//     $insertQuery->bindParam(LINK_PARAM, $link, PDO::PARAM_STR);
+//     $insertQuery->bindParam(TITLE_PARAM, $title, PDO::PARAM_STR);
+//     $insertQuery->bindParam(AUTHOR_PARAM, $author, PDO::PARAM_STR);
+//     $insertQuery->bindParam(MAX_READERS_PARAM, $maxReaders, PDO::PARAM_INT);
+//     $insertQuery->bindParam(MAX_DAYS_PARAM, $maxDays, PDO::PARAM_INT);
+
+//     if (!$insertQuery->execute()) {
+//         http_response_code(500);
+//         exit('<p class="error">Something went wrong!</p>');
+//     }
+
+//     echo "Successfully uploaded resource!";
+
+//     exit();
 ?>
--- src/js/upload.js
+++ src/js/upload.js
@@ -1,30 +1,32 @@
-const upload = () => {
+function upload() {
     var url = '../src/controllers/upload.php';
     uploadCall(url);
 }
 
 function uploadCall(url) {
     const xhr = new XMLHttpRequest();
-    xhr.open('POST', url);
+    xhr.open('FILES', url);
     
-    const form = document.querySelector('#upload-form');
-    let data = new FormData(form);
-    const values = [...data.entries()];
+    // const form = document.querySelector('#upload-form');
+    // let data = new FormData(form);
+    // const values = [...data.entries()];
 
-    var d = "";
-    for (const i of values) {
-        d += '&' + i[0] + '=' + i[1];
-    }
-    d = d.substring(1);
+    // var d = "";
 
-    console.log(d);
+    // // skip resource-type=on and link/file
+    // for (let i = 2; i < values.length; i++) {
+    //     d += '&' + values[i][0] + '=' + values[i][1];
+    // }
+    // d = d.substring(1);
 
-    // set headers
+    // console.log(d);
+
+    // // set headers
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     
     // send request
-    xhr.send(d);
+    xhr.send();
     
     // listen for `load` event
     xhr.onload = () => {
@@ -33,6 +35,21 @@ function uploadCall(url) {
             return;
         }
 
-        form.reset();
+        console.log("resp = ", xhr.responseText);
+
+        // form.reset();
     }
+}
+
+const allowChoosenInput = () => {
+    var linkBtn = document.getElementById("link-btn");
+    var fileBtn = document.getElementById("file-btn");
+    var link = document.getElementById("link");
+    var file = document.getElementById("file");
+
+    link.disabled = !linkBtn.checked;
+    link.style.display = linkBtn.checked ? "block" : "none";
+
+    file.disabled = !fileBtn.checked;
+    file.style.display = fileBtn.checked ? "block" : "none";
 }
