33e0442a034c54a9d7720a3b5999813a3cd36dd8
I568766 <emma.karova@sap.com>
2023-01-29 15:53:52 +0200

    Add No resources found

--- src/js/searchResource.js
+++ src/js/searchResource.js
@@ -1,14 +1,18 @@
 function searchResource() {
     var input = document.getElementById('search').value
-
+   
     input = input.toLowerCase();
     var table = document.getElementById("resources_table");
-  
-    for (var i = 1, row; row = table.rows[i]; i++) {
+    var visibleRowsCounter = table.rows.length - 1;
+ 
+    for (var i = 1, row; row = table.rows[i];  i++) {
         var hide = true;
         for (var j = 0; j < 3; j++) {
             var col = row.cells[j]
-        
+            if (!col) {
+                continue;
+            }
+          
             if (col.innerHTML.toLowerCase().includes(input)) {
                 hide = false;
             }
@@ -16,9 +20,18 @@ function searchResource() {
 
         if (hide) {
             row.style.display = "none";
+            visibleRowsCounter--;
         }
         else {
             row.style.display = "table-row";
+            if (document.contains(document.getElementById("no-resources")) && visibleRowsCounter > 1) {
+                document.getElementById("no-resources").remove();
+            } 
         }
+    }
+
+    if (visibleRowsCounter === 0) {
+        var noDataRow = table.insertRow(-1);
+        noDataRow.innerHTML = "<td id=\"no-resources\" colspan=5 style=\"text-align:center;\">No resources found</td>";
     }
 }
