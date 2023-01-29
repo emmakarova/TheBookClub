77d679be5314367a7841e67b372afc9cdcd2703e
I568766 <emma.karova@sap.com>
2023-01-29 14:44:40 +0200

    Add search to homepage

--- src/js/searchResource.js
+++ src/js/searchResource.js
@@ -1,13 +1,24 @@
 function searchResource() {
-    var url = '../src/controllers/searchResource.php';
-    var el = document.getElementById("resources_table");
-    console.log(el);
-   
+    var input = document.getElementById('search').value
 
-    
-    searchCall(url);   
+    input = input.toLowerCase();
+    var table = document.getElementById("resources_table");
+  
+    for (var i = 1, row; row = table.rows[i]; i++) {
+        var hide = true;
+        for (var j = 0; j < 3; j++) {
+            var col = row.cells[j]
+        
+            if (col.innerHTML.toLowerCase().includes(input)) {
+                hide = false;
+            }
+        }  
+
+        if (hide) {
+            row.style.display = "none";
+        }
+        else {
+            row.style.display = "table-row";
+        }
+    }
 }
-
-function searchCall(url) {
-}
-
